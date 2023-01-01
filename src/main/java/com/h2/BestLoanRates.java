1155f4f15c012312281a5949da17c45b9b193249
Khoa D. Vo <khoa.synthesis@gmail.com>
2023-01-01 22:02:38 +0700

    init project

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,26 @@
+package com.h2;
+
+/**
+ * Hello world!
+ *
+ */
+public class BestLoanRates
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int doubleTheNumber(int number) {
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        return sum;
+    }
+}
